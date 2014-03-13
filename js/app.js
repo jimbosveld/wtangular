77c7110d9d2d93f24f089f9bb9a115ffd98f2afa
jimbosveld <jimdirkwillembosveld@gmail.com>
2014-03-13 17:25:56 +0100

    second big commit

--- js/app.js
+++ js/app.js
@@ -7,15 +7,17 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
-]).
+  'myApp.controllers',
+  'ngTable'
+  ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/info', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/Home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+  $routeProvider.when('/OverOns', {templateUrl: 'partials/overons.html', controller: 'OverOnsCtrl'});
   $routeProvider.when('/stichting_ymere', {templateUrl: 'partials/stichting_ymere.html', controller: 'stichting_ymereCtrl'});
   $routeProvider.when('/woningstichting_rochdale', {templateUrl: 'partials/woningstichting_rochdale.html', controller: 'woningstichting_rochdaleCtrl'});
   $routeProvider.when('/woningstichting_eigen_haard', {templateUrl: 'partials/woningstichting_eigen_haard.html', controller: 'woningstichting_eigen_haardCtrl'});
   $routeProvider.when('/stadgenoot', {templateUrl: 'partials/stadgenoot.html', controller: 'stadgenootCtrl'});
   $routeProvider.when('/woonstichting_de_key', {templateUrl: 'partials/woonstichting_de_key.html', controller: 'woonstichting_de_keyCtrl'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/Home'});
 }]);
 
--- js/controllers.js
+++ js/controllers.js
@@ -2,103 +2,184 @@
 
 // PAGE CONTROLLERS //
 
-angular.module('myApp.controllers', [])
+angular.module('myApp.controllers', ['ngTable'])
 
-.controller('MyCtrl1', [
+.controller('OverOnsCtrl', [
 
-  function() {}
-  ])
+    function() {}
+    ])
 .controller('stichting_ymereCtrl', [
 
-  function() {}
-  ])
+    function() {}
+    ])
 .controller('woningstichting_rochdaleCtrl', [
 
-  function() {}
-  ])
+    function() {}
+    ])
 .controller('woningstichting_eigen_haardCtrl', [
 
-  function() {}
-  ])
+    function() {}
+    ])
 .controller('stadgenootCtrl', [
 
-  function() {}
-  ])
+    function() {}
+    ])
 .controller('woonstichting_de_keyCtrl', [
-
-  function() {}
-  ])
-.controller('wocosLijst', ['$scope', function ($scope) {
-  $scope.woningcorporaties = [
-  {naam:"Ymere",},
-  {naam:"Rochdale"}
-  ]
-}]);
+    function() {}
+    ])
+.controller('HomeCtrl', [
+    function() {}
+    ]);
 
 
-// SEARCH MODULE & SCOPE //
+// RANKING CONTROLLER
 
-
-
-// WOCO CONTROLLERS OPHALEN JSON //
-
-function WocoCtrlYmere($scope, $http) {
-  $scope.url = "https://onderzoek.thebrighthouse.nl/woontevreden/api/corporatie/stichting_ymere/data.json";
-  $scope.gegevens = [];
-  $scope.HandleWocosYmere = function(data, status) {
-    $scope.gegevens = data;
-  }
-  $scope.fetch = function() {
-    $http.get($scope.url).success($scope.HandleWocosYmere);
-  }
-  $scope.fetch();
+function RankingCtrl($scope, $http, ngTableParams, $filter) {
+    $scope.url = "http://api.woontevreden.nl/data/corporatiecijfers";
+    $scope.gegevens = []
+    $scope.HandleRanking = function(data, status) {
+        $scope.gegevens = data;
+        $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 300,
+            sorting: {
+                corporatie: 'desc' // initial sorting
+            }
+        }, {
+            total: data.length,
+            getData: function($defer, params) {
+                var filteredData = params.filter() ? $filter('filter')(data, params.filter()) : data;
+                var orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : data;
+                params.total(orderedData.length);
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+    }
+    $scope.fetch = function() {
+        $http.get($scope.url).success($scope.HandleRanking);
+    }
+    $scope.fetch()
 }
 
-function WocoCtrlRochdale($scope, $http) {
-  $scope.url = "https://onderzoek.thebrighthouse.nl/woontevreden/api/corporatie/woningstichting_rochdale/data.json";
-  $scope.gegevens = [];
-  $scope.HandleWocosRochdale = function(data, status) {
-    $scope.gegevens = data;
-  }
-  $scope.fetch = function() {
-    $http.get($scope.url).success($scope.HandleWocosRochdale);
-  }
-  $scope.fetch();
+// WOCO CONTROLLERS//
+
+function WocoCtrlYmere($scope, $http, ngTableParams, $filter) {    
+    $scope.url = "http://api.woontevreden.nl/data/corporaties/2070/reviews";
+    $scope.gegevens = [];    
+    $scope.HandleWocosYmere = function(data, status) {
+        $scope.gegevens = data;
+        $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 300,
+        }, {
+            total: data.length,
+            getData: function($defer, params) {
+                var filteredData = params.filter() ? $filter('filter')(data, params.filter()) : data;
+                var orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : data;
+                params.total(orderedData.length);
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));                
+            }
+        }
+        );
+    }
+    $scope.fetch = function() {
+        $http.get($scope.url).success($scope.HandleWocosYmere);        
+    }
+    $scope.fetch()    
 }
 
-function WocoCtrlKey($scope, $http) {
-  $scope.url = "https://onderzoek.thebrighthouse.nl/woontevreden/api/corporatie/woonstichting_de_key/data.json";
-  $scope.gegevens = [];
-  $scope.HandleWocosRochdale = function(data, status) {
-    $scope.gegevens = data;
-  }
-  $scope.fetch = function() {
-    $http.get($scope.url).success($scope.HandleWocosRochdale);
-  }
-  $scope.fetch();
+function WocoCtrlRochdale($scope, $http, ngTableParams, $filter) {
+    $scope.url = "http://api.woontevreden.nl/data/corporaties/17/reviews";
+    $scope.gegevens = []
+    $scope.HandleWocosRochdale = function(data, status) {
+        $scope.gegevens = data;
+        $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 300,
+        }, {
+            total: data.length,
+            getData: function($defer, params) {
+                var filteredData = params.filter() ? $filter('filter')(data, params.filter()) : data;
+                var orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : data;
+                params.total(orderedData.length);
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+    }
+    $scope.fetch = function() {
+        $http.get($scope.url).success($scope.HandleWocosRochdale);
+    }
+    $scope.fetch()
 }
 
-function WocoCtrlStadgenoot($scope, $http) {
-  $scope.url = "https://onderzoek.thebrighthouse.nl/woontevreden/api/corporatie/stadgenoot/data.json";
-  $scope.gegevens = [];
-  $scope.HandleWocosRochdale = function(data, status) {
-    $scope.gegevens = data;
-  }
-  $scope.fetch = function() {
-    $http.get($scope.url).success($scope.HandleWocosRochdale);
-  }
-  $scope.fetch();
+function WocoCtrlKey($scope, $http, ngTableParams, $filter) {
+    $scope.url = "http://api.woontevreden.nl/data/corporaties/2103/reviews";
+    $scope.gegevens = []
+    $scope.HandleWocosKey = function(data, status) {
+        $scope.gegevens = data;
+        $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 300,
+        }, {
+            total: data.length,
+            getData: function($defer, params) {
+                var filteredData = params.filter() ? $filter('filter')(data, params.filter()) : data;
+                var orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : data;
+                params.total(orderedData.length);
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+    }
+    $scope.fetch = function() {
+        $http.get($scope.url).success($scope.HandleWocosKey);
+    }
+    $scope.fetch()
 }
 
-function WocoCtrlEigenHaard($scope, $http) {
-  $scope.url = "https://onderzoek.thebrighthouse.nl/woontevreden/api/corporatie/woningstichting_eigen_haard/data.json";
-  $scope.gegevens = [];
-  $scope.HandleWocosRochdale = function(data, status) {
-    $scope.gegevens = data;
-  }
-  $scope.fetch = function() {
-    $http.get($scope.url).success($scope.HandleWocosRochdale);
-  }
-  $scope.fetch();
+function WocoCtrlStadgenoot($scope, $http, ngTableParams, $filter) {
+    $scope.url = "http://api.woontevreden.nl/data/corporaties/124/reviews";
+    $scope.gegevens = []
+    $scope.HandleWocosStadgenoot = function(data, status) {
+        $scope.gegevens = data;
+        $scope.tableParams = new ngTableParams({
+            page: 1,
+            count:300,
+        }, {
+            total: data.length,
+            getData: function($defer, params) {
+                var filteredData = params.filter() ? $filter('filter')(data, params.filter()) : data;
+                var orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : data;
+                params.total(orderedData.length);
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+    }
+    $scope.fetch = function() {
+        $http.get($scope.url).success($scope.HandleWocosStadgenoot);
+    }
+    $scope.fetch()
 }
 
+function WocoCtrlEigenHaard($scope, $http, ngTableParams, $filter) {
+    $scope.url = "http://api.woontevreden.nl/data/corporaties/108/reviews";
+    $scope.gegevens = []
+    $scope.HandleWocosEigenHaard = function(data, status) {
+        $scope.gegevens = data;
+        $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 300,
+        }, {
+            total: data.length,
+            getData: function($defer, params) {
+                var filteredData = params.filter() ? $filter('filter')(data, params.filter()) : data;
+                var orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : data;
+                params.total(orderedData.length);
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+    }
+    $scope.fetch = function() {
+        $http.get($scope.url).success($scope.HandleWocosEigenHaard);
+    }
+    $scope.fetch()
+}
